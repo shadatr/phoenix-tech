1056993d1c72bbe2d1ea603910e138d77a0e4b2a
shdab <130516923+shadatr@users.noreply.github.com>
2024-02-18 18:37:52 +0300

    logo

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from './redux/store.ts';
 import { Provider } from 'react-redux';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
     <Provider store={store}>
-
     <App />
     </Provider>
   </Router>
